903e7ac13b9d09a9ef14b9aa47c3d3960ee22d00
Davipcrs <davi.coelho_comp@outlook.com>
2024-10-16 23:42:27 -0300

    Dir_utils init

---
+++ utils/dir_utils.h
@@ -0,0 +1,11 @@
+#ifndef DIR_UTILS_H
+#define DIR_UTILS_H
+
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <ctype.h>
+#include "../consts_def.h"
+
+#endif
